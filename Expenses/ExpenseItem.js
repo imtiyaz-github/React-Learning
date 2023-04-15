7b14766e3b9e1e139c6e4daccf988dc17e6ee949
IMTIYAZ M SHIRASANGI <117428054+imtiyaz-github@users.noreply.github.com>
2023-04-15 15:05:07 +0530

    Addedd buttons changes

--- Expenses/ExpenseItem.js
+++ Expenses/ExpenseItem.js
@@ -1,17 +1,38 @@
-import React from 'react';
+import React, {useState}from 'react';
 
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
 import './ExpenseItem.css';
 
 const ExpenseItem = (props) => {
+
+  const [title, setTitle] = useState(props.title);
+  const [amount,setAmout] = useState(props.amount)
+  console.log('ExpenseItem evaluatd')
+
+  const clickHandler = () => {
+    setTitle('Updated!');
+     console.log(title);
+   
+     } 
+      const changeAmoutHandler = () => {
+        setAmout('100');
+        console.log(amount);
+      }
+
   return (
-    <Card className='expense-item'>
+    <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className='expense-item__description'>
-        <h2>{props.title}</h2>
-        <div className='expense-item__price'>${props.amount}</div>
+        <h2>{title}</h2>
+        <div className='expense-item__price'>${amount}</div>
       </div>
+      <button onClick={clickHandler} style={{
+        padding : "10px", borderRadius: "5px " , margin: '10px', background: "#40005d",color: 'white' ,cursor:"pointer"}}>Change Title</button>
+      <button onClick={changeAmoutHandler} style={{
+        padding : "10px", borderRadius: "5px " , margin: '10px', background: "#40005d",color: 'white' ,cursor:"pointer"
+      }}
+      >Change Amout</button>
     </Card>
   );
 }
