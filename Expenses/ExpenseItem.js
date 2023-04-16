6be50294e4665c8804e070f26d1ee7037896d485
IMTIYAZ M SHIRASANGI <117428054+imtiyaz-github@users.noreply.github.com>
2023-04-16 22:08:12 +0530

    Fiinished and add made some changes

--- Expenses/ExpenseItem.js
+++ Expenses/ExpenseItem.js
@@ -1,38 +1,40 @@
-import React, {useState}from 'react';
+import React from 'react';
 
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
 import './ExpenseItem.css';
 
-const ExpenseItem = (props) => {
 
-  const [title, setTitle] = useState(props.title);
-  const [amount,setAmout] = useState(props.amount)
-  console.log('ExpenseItem evaluatd')
 
-  const clickHandler = () => {
-    setTitle('Updated!');
-     console.log(title);
+  // const [title, setTitle] = useState(props.title);
+  // const [amount,setAmout] = useState(props.amount)
+  // console.log('ExpenseItem evaluated');
+
+  // const clickHandler = () => {
+  //   setTitle('Updated!');
+  //    console.log(title);
    
-     } 
-      const changeAmoutHandler = () => {
-        setAmout('100');
-        console.log(amount);
-      }
+  //    } 
+  //     const changeAmoutHandler = () => {
+  //       setAmout('100');
+  //       console.log(amount);
+  //     }
 
+  const ExpenseItem = (props) => {
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className='expense-item__description'>
-        <h2>{title}</h2>
-        <div className='expense-item__price'>${amount}</div>
+        <h2>{props.title}</h2>
+        <div className='expense-item__price'>${props.amount}</div>
       </div>
-      <button onClick={clickHandler} style={{
+      {/* <button onClick={clickHandler} style={{
         padding : "10px", borderRadius: "5px " , margin: '10px', background: "#40005d",color: 'white' ,cursor:"pointer"}}>Change Title</button>
       <button onClick={changeAmoutHandler} style={{
-        padding : "10px", borderRadius: "5px " , margin: '10px', background: "#40005d",color: 'white' ,cursor:"pointer"
-      }}
-      >Change Amout</button>
+        padding : "10px", borderRadius: "5px " , margin: '10px', background: "#40005d",color: 'white' ,cursor:"pointer" */}
+{/*       
+      >Change Amout</button> */}
+  
     </Card>
   );
 }
